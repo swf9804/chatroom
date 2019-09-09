36e9c9ca3b38f1341cc1edee97b5de3269846427
swf <1043197615@qq.com>
2019-09-09 23:51:20 +0800

    Initial commit

---
+++ src/main/java/com/example/websocket/chatroom/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.example.websocket.chatroom.config;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * @author SWF
+ * @date 2019/9/9 23:47
+ **/
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
