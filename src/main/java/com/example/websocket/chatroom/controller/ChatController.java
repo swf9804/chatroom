4c8daeab938eb231ee96b304e2993f106f67a801
swf <1043197615@qq.com>
2019-09-10 00:29:22 +0800

    创建Controller 用以接收和发送消息

---
+++ src/main/java/com/example/websocket/chatroom/controller/ChatController.java
@@ -0,0 +1,29 @@
+package com.example.websocket.chatroom.controller;
+
+import com.example.websocket.chatroom.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+
+/**
+ * @author SWF
+ * @date 2019/9/10 0:02
+ **/
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor){
+        headerAccessor.getSessionAttributes().put("username",chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
